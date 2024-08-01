13a090ffe130dea6dd3bef263ec4f49266696f11
Bladforceone <bladforceone@gmail.com>
2024-08-01 23:30:39 +0700

    Fix 3

--- src/controller/MainController.h
+++ src/controller/MainController.h
@@ -4,7 +4,13 @@
 
 #ifndef MAINCONTROLLER_H
 #define MAINCONTROLLER_H
-#include "../main.cpp"
+enum Buttons {
+    UpButton = 1 << 0,
+    LeftButton = 1 << 1,
+    DownButton = 1 << 2,
+    RightButton = 1 << 3,
+    EscButton = 1 << 4,
+};
 
 class MainController {
 public:
--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,5 @@
 #include <iostream>
 #include "controller/MainController.h"
-enum Buttons {
-    UpButton = 1 << 0,
-    LeftButton = 1 << 1,
-    DownButton = 1 << 2,
-    RightButton = 1 << 3,
-    EscButton = 1 << 4,
-};
 int main() {
 
 }
